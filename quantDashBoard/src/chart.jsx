8c99dcf5879cc89104ded2d573fa3874a8fb4b7b
Rhys Jervis <rhys.jervis94@gmail.com>
2025-06-19 14:14:21 -0400

    add DashboardLayout, TabPanel, Chart, and Navbar components with styles

---
+++ quantDashBoard/src/chart.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./chart.css";
+
+function Chart() {
+    return (
+        <div className="chart">
+        <h2>Chart Component</h2>
+        <p>This is where the chart will be displayed.</p>
+        {/* Add your charting library component here, e.g., Chart.js, Recharts, etc. */}
+        </div>
+    );
+    }
+
+export default Chart;
