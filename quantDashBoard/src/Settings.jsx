5e17fba0f9de4e9a77c694e890404619c52ff783
Rhys Jervis <rhys.jervis94@gmail.com>
2025-07-11 13:46:57 -0400

    Created settings component

---
+++ quantDashBoard/src/Settings.jsx
@@ -0,0 +1,15 @@
+import "./Settings.css";
+
+
+function Settings() {
+
+  return (
+    <div className="settings">
+      <h1>Settings</h1>
+      <p></p>
+    </div>
+  );
+}
+
+export default Settings;
+
